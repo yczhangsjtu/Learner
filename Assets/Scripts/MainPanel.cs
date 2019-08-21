b77fef79ee498168a9f9dd55c45178a06cd61867
Yuncong Zhang <yuncong.zhang@unity3d.com>
2019-08-21 13:48:49 +0800

    Establish the main script structure.

---
+++ Assets/Scripts/MainPanel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.UIWidgets.animation;
+using Unity.UIWidgets.engine;
+using Unity.UIWidgets.foundation;
+using Unity.UIWidgets.material;
+using Unity.UIWidgets.painting;
+using Unity.UIWidgets.widgets;
+using UnityEngine;
+
+public class MainPanel : UIWidgetsPanel
+{
+    
+    protected override Widget createWidget() {
+        return new WidgetsApp(
+            home: new MainApp(),
+            pageRouteBuilder: (RouteSettings settings, WidgetBuilder builder) =>
+                new PageRouteBuilder(
+                    settings: settings,
+                    pageBuilder: (BuildContext context, Animation<float> animation,
+                        Animation<float> secondaryAnimation) => builder(context)
+                )
+        );
+    }
+
+    class MainApp : StatefulWidget {
+        public MainApp(Key key = null) : base(key) {
+        }
+
+        public override State createState() {
+            return new MainAppState();
+        }
+    }
+
+    class MainAppState : State<MainApp> {
+        int counter = 0;
+
+        public override Widget build(BuildContext context) {
+            return new Scaffold(
+                appBar: new AppBar()
+            );
+        }
+    }
+}
